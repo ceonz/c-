755145d8950b96ef3ffc8022df4c5be5a688ea3c
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-01 11:49:15 -0500

    Lab quiz1

---
+++ 01_I love c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter an int: ";
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+        cout << "I love c++" << endl;
+    return 0;
+}
