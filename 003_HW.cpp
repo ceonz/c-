03590a6b088bcbe02dc0978d57f570e558ed92bd
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-07 22:30:29 -0500

    hw 003

---
+++ 003_HW.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string air = "air";
+    string fire = "fire";
+    string water = "water";
+
+    cout << air << endl;
+    cout << fire << endl;
+    cout << water << endl;
+
+    return 0;
+}
