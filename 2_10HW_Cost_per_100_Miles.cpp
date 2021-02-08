381c99e00d5863396ff5c65f0915b647dacfa6a4
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-07 21:52:32 -0500

    fix error

--- 2_10HW_Cost_per_100_Miles.cpp
+++ 2_10HW_Cost_per_100_Miles.cpp
@@ -8,14 +8,15 @@ Then print the cost per 100 miles and how far the car can go with the gas in the
 */
 
 #include <iostream>
+#include <iomanip> //steam manipulator for set precision
 
 using namespace std;
 
 int main()
 {
-    int gallons = 0;
+    float gallons = 0;
     float fuel_efficiency = 0;
-    int price = 0;
+    float price = 0;
 
     //prompt in c++
     cout << "The number of gallons of gas in the tank: ";
@@ -29,13 +30,14 @@ int main()
 
     //cost = (100 miles / fuel efficiency = gallons used for 100 miles) * price per gallon
     // if you are operating with divisions always declare variable as float
-    float cost = (100 / fuel_efficiency) * price;
+    float cost = (100.0 / fuel_efficiency) * price;
 
     //distance_traveled = current gallon * fuel efficiency
-    int distance = gallons * fuel_efficiency;
+    float distance = gallons * fuel_efficiency;
+    cout << fixed; //to show results in decimals (fixed)
 
-    cout << cost << endl;
-    cout << distance << endl;
+    cout << setprecision(2) << cost << endl;
+    cout << setprecision(2) << distance << endl;
 
     return 0;
 }
