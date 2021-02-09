f6fa0fecc14e3c9f7b7b93bbdcbb2f3c0ef98a62
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-08 22:20:44 -0500

    project1 first commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s;
+    int sum = 0;
+
+    while (cin >> s)
+    {
+        sum = sum + s;
+    }
+    cout << sum << endl;
+
+    return 0;
+}
