aea36a1ce8a7aa12100f27aaecafa8425edf1391
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-08 22:30:04 -0500

    add comment

--- sum.cpp
+++ sum.cpp
@@ -1,3 +1,14 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Project 1
+
+using redirection to receive and read integers from randomly generated text file.
+using while loop to iterate through numbers and outputs sum
+
+*/
+
 #include <iostream>
 using namespace std;
 
