924156e5d023d979c100e9084a09805e6f9c8d31
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-24 15:36:29 -0500

    first commit

---
+++ checkerboard.cpp
@@ -0,0 +1,34 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Lab 4B
+
+Print out starts receiving prompts (row col)
+by putting a space in between *
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int width, height;
+    cout << "Please input width: ";
+    cin >> width;
+    cout << "Please input height: ";
+    cin >> height;
+
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            if ((row + col) % 2 == 0) { // row and col are odd or even at the same time.
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
