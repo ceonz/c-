1b01e6ac46c0bf5da177b32d562b0171d980ddcb
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-24 16:04:25 -0500

    first commit

---
+++ lower.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Lab 4D
+
+use for loop to create a
+increment of *'s
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+    cout << "Please input size: ";
+    cin >> size;
+
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (row <= col) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
