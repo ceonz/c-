7842ab4bf23adabacc62119183a7c54b7c967757
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-24 15:54:49 -0500

    first commit

---
+++ cross.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Lab 4C
+
+use for loop to create
+a cross shape with *
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+    cout << "Please input size: ";
+    cin >> size;
+
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (row == col or row + col == size + 1) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
