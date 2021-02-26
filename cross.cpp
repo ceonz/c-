6c4d68cf42a659c8a66b3d27022e7637e1bdd08b
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-26 13:04:04 -0500

    fix error - iteration count 0

--- cross.cpp
+++ cross.cpp
@@ -14,12 +14,14 @@ a cross shape with *
 using namespace std;
 int main() {
     int size;
-    cout << "Please input size: " << endl;
+    cout << "Please input size: ";
     cin >> size;
+    cout << endl;
 
+    // be careful! iteration starts from 0 not 1
     for (int row = 0; row < size; row++) {
         for (int col = 0; col < size; col++) {
-            if (row == col or row + col == size + 1) {
+            if (row == col or row + col == size - 1) {
                 cout << "*";
             }
             else {
