4fb83c25891552f919968641f1222be0785af1b9
ceonz <45135014+ceonz@users.noreply.github.com>
2021-02-26 12:27:07 -0500

    Fix error - endl

--- box.cpp
+++ box.cpp
@@ -17,6 +17,7 @@ int main() {
     cin >> width;
     cout << "Please input height: ";
     cin >> height;
+    cout << endl;
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
