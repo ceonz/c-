e48c55a5d9d756d7050a2ffbb762e8d1c0c54c05
ceonz <45135014+ceonz@users.noreply.github.com>
2021-03-05 19:13:00 -0500

    first commit

---
+++ numbers.cpp
@@ -0,0 +1,43 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Lab - 5B
+
+create a function that if n is prime number then
+return true, otherwise return false
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function prototypes
+bool isDivisibleBy(int n, int d);
+bool isPrime(int n);
+
+int main() {
+    cout << isPrime(13) << endl;
+    cout << isPrime(6) << endl;
+}
+
+bool isPrime(int n) {
+    for (int i = 2; i < n / 2; i++) {
+        if (isDivisibleBy(n, i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool isDivisibleBy(int n, int d) {
+    if (d == 0) {
+        return false;
+    }
+    if (n % d == 0) {
+        return true;
+    }
+    return false;
+}
