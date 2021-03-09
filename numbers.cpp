dd6a94e9f02819bef187e29e2856524169117a02
ceonz <45135014+ceonz@users.noreply.github.com>
2021-03-09 00:07:50 -0500

    Fix error - wrong file

---
+++ numbers.cpp
@@ -0,0 +1,59 @@
+/*
+Author: Ceon Bang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Lab - 5C
+
+Write a function that returns smallest prime greater than n
+for example) nextPrime(14) == 17
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+// Function prototypes
+bool isDivisibleBy(int n, int d);
+bool isPrime(int n);
+int nextprime(int n);
+
+int nextprime(int n) {
+    //Increment n 1 by 1 until you find a prime
+    n++; // Increment n by 1 first. To avoid the case that n is a lso a prime number
+    while (!isPrime(n)) {
+        n++; // while n is not a prime, keep incrementing n by 1
+    }
+    return n;
+}
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (isDivisibleBy(n, i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool isDivisibleBy(int n, int d) {
+    if (d == 0) {
+        return false;
+    }
+    if (n % d == 0) {
+        return true;
+    }
+    return false;
+}
+
+
+int main() {
+    cout << nextprime(17) << endl;
+    cout << nextprime(3) << endl;
+    return 0;
+}
