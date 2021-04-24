37e3ca8050e4c11c0b3926dd3ad92048a45f07ff
ceonz <45135014+ceonz@users.noreply.github.com>
2021-04-23 22:37:46 -0400

    fix - error - formatting

--- poem.cpp
+++ poem.cpp
@@ -12,14 +12,14 @@
 using namespace std;
 
 string* createAPoemDynamically() {
-    string* p = new string;
+    string *p = new string;
     *p = "Roses are red, violets are blue";
     return p;
 }
 
 int main() {
     while (true) {
-        string* p;
+        string *p;
         p = createAPoemDynamically();
 
         // assume that the poem p is not needed at this point
