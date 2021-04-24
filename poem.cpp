dd4578823d81762d8ee5708aae910626f4810987
ceonz <45135014+ceonz@users.noreply.github.com>
2021-04-23 21:55:22 -0400

    first commit

---
+++ poem.cpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Ceon Bang
+ * Course: CSCI-136
+ * Instructor: Tong Yi
+ * Assignment: LAB9D
+ *
+ * Here, briefly, at least in one or a few sentences
+ * describe what the program does.
+ */
+
+#include <iostream>
+using namespace std;
+
+string* createAPoemDynamically() {
+    string* p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
+}
+
+int main() {
+    while (true) {
+        string* p;
+        p = createAPoemDynamically();
+
+        // assume that the poem p is not needed at this point
+        delete p;
+
+    }
+}
